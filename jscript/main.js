e7db377b1572425a8e46af2e22b666a8bc3bc880
Tappobill <christian.salvati@hotmail.it>
2022-10-18 16:06:08 +0200

    Completo

---
+++ jscript/main.js
@@ -0,0 +1,12 @@
+let name = prompt ( " Write your name " )
+console.log ( name );
+
+let lastName = prompt ( " Write your last name " )
+console.log ( lastName );
+
+let favouriteColor = prompt ( " Write your favourite color " )
+console.log ( favouriteColor );
+
+const number = 7;
+
+document.getElementById ("password") .innerHTML = `Your password is: ${ name }${ lastName }${ favouriteColor }${ number }`
