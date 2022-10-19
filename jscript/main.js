75aedfb00045bbdef3485479c4927618dea9190d
Tappobill <christian.salvati@hotmail.it>
2022-10-19 11:54:36 +0200

    Indentazione

--- jscript/main.js
+++ jscript/main.js
@@ -1,12 +1,12 @@
-let name = prompt ( " Write your name " )
-console.log ( name );
+let name = prompt(" Write your name ")
+console.log(name);
 
-let lastName = prompt ( " Write your last name " )
-console.log ( lastName );
+let lastName = prompt(" Write your last name ")
+console.log(lastName);
 
-let favouriteColor = prompt ( " Write your favourite color " )
-console.log ( favouriteColor );
+let favouriteColor = prompt(" Write your favourite color ")
+console.log(favouriteColor);
 
 const number = 7;
 
-document.getElementById ("password") .innerHTML = `Your password is: ${ name }${ lastName }${ favouriteColor }${ number }`
+document.getElementById("password").innerHTML = `Your password is: ${name}${lastName}${favouriteColor}${number}`
